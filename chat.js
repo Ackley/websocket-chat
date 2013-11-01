eef696c1fb22abc460b4fb1ad38bf8abdf3b01c5
Ackley <mi13a006@oecu.jp>
2013-11-01 16:16:31 +0900

    チャットの発言機能を実装

--- chat.js
+++ chat.js
@@ -6,3 +6,19 @@ var app = express();
 
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+
+app.post('/post_message', function(req, res){
+    var name = "";
+    var message = "";
+    if(req.body.name)
+	name = req.body.name;
+    if(req.body.message)
+	message = req.body.message;
+
+    //key:value, key:value
+    db.rpush('chat-log', JSON.stringify({name: name, message: message}));
+    
+});
+
+app.listen(3000);
+console.log("PORT:3000");
